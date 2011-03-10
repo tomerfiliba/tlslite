f914d33eb04f6053ec0535e477cc35f9c19c08a1
Tomer Filiba <tomerf@il.ibm.com>
2011-03-10 23:28:17 +0200

    adding readme

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-rm -r installers &> /dev/null
-mkdir installers
-
 rm -r build dist &> /dev/null
 python setup.py sdist --formats=zip,gztar
 cp dist/* installers/
@@ -15,8 +12,13 @@ rm -r build dist &> /dev/null
 /c/Python26/python setup.py bdist_wininst 
 cp dist/* installers/
 
+# requires Visual Studio 2003!
 # rm -r build dist &> /dev/null
 # /c/Python25/python setup.py bdist_wininst 
 # cp dist/* installers/
 
 rm -r build dist &> /dev/null
+
+echo "uploading to sourceforge, press CTRL+C to abort"
+sleep 10
+scp installers/* gangesmaster,rpyc@frs.sourceforge.net:/home/frs/project/r/rp/rpyc/tlslite/
